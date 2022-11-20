ff1f972b9b59530c9ea0714d3ddb4dc832961c5b
Aleksandr Khachich <95611562+Khanchic@users.noreply.github.com>
2022-11-20 11:53:54 +0200

    finally

---
+++ tasks/task-10.js
@@ -0,0 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const inputNum = document.querySelector("input");
+const btnCreate = document.querySelector("[data-create]");
+const btnDest = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+btnCreate.addEventListener("click", createBoxes);
+
+function createBoxes(e) {
+  console.log(boxes);
+  const childrenNum = boxes.childNodes.length;
+  let width = 30 + 10 * childrenNum;
+  let height = 30 + 10 * childrenNum;
+  for (let i = 1; i <= inputNum.value; i++) {
+    const hexColor = getRandomHexColor();
+    boxes.insertAdjacentHTML(
+      "beforeend",
+      `<div style="background-color:${hexColor}; width: ${width}px; height:${height}px;"> </div>`
+    );
+    width += 10;
+    height += 10;
+  }
+}
+btnDest.addEventListener("click", (e) => {
+  boxes.innerHTML = "";
+});
