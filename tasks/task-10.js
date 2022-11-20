766cd3266ff7937b58e753b5622e26d41b3693f7
Aleksandr Khachich <95611562+Khanchic@users.noreply.github.com>
2022-11-20 17:09:06 +0200

    плачу

--- tasks/task-10.js
+++ tasks/task-10.js
@@ -7,21 +7,22 @@ const btnDest = document.querySelector("[data-destroy]");
 const boxes = document.querySelector("#boxes");
 btnCreate.addEventListener("click", createBoxes);
 
-function createBoxes(e) {
-  console.log(boxes);
-  const childrenNum = boxes.childNodes.length;
-  let width = 30 + 10 * childrenNum;
-  let height = 30 + 10 * childrenNum;
-  for (let i = 1; i <= inputNum.value; i++) {
-    const hexColor = getRandomHexColor();
-    boxes.insertAdjacentHTML(
-      "beforeend",
-      `<div style="background-color:${hexColor}; width: ${width}px; height:${height}px;"> </div>`
-    );
-    width += 10;
-    height += 10;
-  }
-}
+console.log("Hello world");
+// function createBoxes(e) {
+//   console.log(boxes);
+//   const childrenNum = boxes.childNodes.length;
+//   let width = 30 + 10 * childrenNum;
+//   let height = 30 + 10 * childrenNum;
+//   for (let i = 1; i <= inputNum.value; i++) {
+//     const hexColor = getRandomHexColor();
+//     boxes.insertAdjacentHTML(
+//       "beforeend",
+//       `<div style="background-color:${hexColor}; width: ${width}px; height:${height}px;"> </div>`
+//     );
+//     width += 10;
+//     height += 10;
+//   }
+// }
 btnDest.addEventListener("click", (e) => {
   boxes.innerHTML = "";
 });
